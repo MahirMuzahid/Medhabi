c07e2d86c92d218d4186c9bac007c0d3218d3c59
Mahir Muzahid <muzahid12@live.com>
2020-04-18 11:00:02 +0600

    Medhabi Created

---
+++ login/Data/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace login.Data
+{
+    public class Response
+    {
+        public string  message { get; set; }
+        public int status { get; set; }
+    }
+}
---
+++ login/Data/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace login.Data
+{
+    public class Student
+    {
+        public int studentID { get; set; }
+        public string mail { get; set; }
+        public string password { get; set; }
+        public string name { get; set; }
+        public string phonenumber { get; set; }
+        public int amount { get; set; }
+        public int point { get; set; }
+        public string rank { get; set; }
+        public int total_question_answered { get; set; }
+        public int total_right_answer { get; set; }
+        
+    }
+}
